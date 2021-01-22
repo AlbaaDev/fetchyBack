0c1f0c1f5efa3a2e4b1178db53d07d90e243688c
FazliuArber <abi-boys@live.fr>
2021-01-22 20:37:18 +0100

    init

---
+++ src/main/java/com/web/fetchyback/FetchyBackApplication.java
@@ -0,0 +1,13 @@
+package com.web.fetchyback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FetchyBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FetchyBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/web/FetchyBack/FetchyBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.FetchyBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FetchyBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
